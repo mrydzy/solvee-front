5c8fa75d782e26a0afb03575e569610ddd091794
mrydzy <majrydzy@gmail.com>
2016-03-06 19:45:09 +0100

    styles

--- dist/create-map-logic.js
+++ dist/create-map-logic.js
@@ -3,8 +3,19 @@ $('document').ready(function() {
     var textAreaId = 'text' + $(this).attr('data-target');
     return $(this).prepend('<textarea id= ' + textAreaId + ' class="col-input" placeholder="Placeholder. Add text to keep, leave empty to remove.">').addClass('map-placeholder');
   });
-
   autosize($('textarea'));
+  $('textarea').blur(function() {
+    console.log('lost focus', $(this).parent());
+    var wrapper = $(this).parent();
+    if ($.trim($(this).val())) { //textarea is not empty
+      wrapper.removeClass('map-placeholder');
+    } else {//textarea is empty
+      wrapper.addClass('map-placeholder');
+    }
+  });
+
+
+
 });
 
 
