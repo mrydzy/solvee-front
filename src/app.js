3aca052d542eccb741580b544d540b5dcc6a08b8
mrydzy <majrydzy@gmail.com>
2016-03-21 20:21:17 +0100

    config fix

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 const app = express();
 
 function configure(cfg) {
   app.set('cfg', cfg);
 
-  app.set('views', cfg.DIR);
+  app.set('views', path.join(cfg.DIR, 'views'));
   app.set('view engine', 'jade');
 
   app.use(require('./routes'));
