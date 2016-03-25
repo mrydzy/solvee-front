6e80f0c568f35a268653cb24af9f3d843f5532d9
mrydzy <majrydzy@gmail.com>
2016-03-25 19:12:24 +0100

    signing the header

--- src/js/map-service.js
+++ src/js/map-service.js
@@ -1,6 +1,6 @@
 const $ = require('jquery');
+const readCookie = require('./services').readCookie;
 const backendUrl = "http://localhost:3300/trees";
-
 
 function sendTree(data) {
   send("POST", data, function(){}, "");
@@ -14,12 +14,28 @@ function getTree(treeId, success) {
   $.getJSON(backendUrl + treeId, success);
 }
 
+function addCredentials(headers) {
+  var credentials = readCookie('credentials');
+  if (credentials) {
+    headers.Authorization = "Bearer " + credentials;
+  }
+  return headers;
+}
+
+function getHeaders() {
+  var headers = {};
+  addCredentials(headers);
+  console.log('using headers', headers);
+  return headers;
+}
+
 function send(type, data, success, suffix) {
   $.ajax({
     type: type,
     url: backendUrl + suffix,
     data: data,
     success: success,
+    headers: getHeaders(),
     dataType: 'application/json'
   });
 }
