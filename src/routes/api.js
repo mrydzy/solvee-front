60c17c995af9eb6f9cfe2a0c458efd8af21574f4
mrydzy <majrydzy@gmail.com>
2016-03-25 19:16:51 +0100

    redundant comment

--- src/routes/api.js
+++ src/routes/api.js
@@ -4,23 +4,6 @@ const fetch = require('isomorphic-fetch');
 
 const backendUrl = "http://localhost:3300";
 
-//router.get('/:id', function(req, res) {
-//  var treeId = req.params.id;
-//  fetch(backendUrl + '/' + treeId)
-//    .then(function(response) {
-//      if (response.status >= 400) {
-//        throw new Error("Bad response from server");
-//      }
-//      return response.json();
-//    })
-//    .then(function(response) {
-//      console.log(response.data);
-//      res.send(JSON.parse(response.data));
-//    });
-//
-//});
-
-
 router.get('/login', function(req, res) {
   res.sendFile( 'login.html', {root: req.app.locals.settings.cfg.DIR + '/views'});
 
