d3b7efcf77d12d4f81b3151e5595ef330f75a487
mrydzy <majrydzy@gmail.com>
2016-03-28 17:38:07 +0200

    clean tree twins fix

--- src/js/create-map-logic.js
+++ src/js/create-map-logic.js
@@ -166,7 +166,7 @@ function filterEmpty(array) {
 
 function distributeTwins(node) { //if there are just 2 children, 1 should be set!
   if ((node.children[0].id % 10) !== 1) { //with twins, the first one has to be set
-    updateId(node.id*10+1, node.children[1], 1);
+    updateId(node.id*10+1, node.children[0], 1);
   }
   if ((node.children[1].id % 10) !== 3) { //with twins, the first one has to be set
     updateId(node.id*10+3, node.children[1], 3);
@@ -176,7 +176,7 @@ function distributeTwins(node) { //if there are just 2 children, 1 should be set
 function updateId(newId, node, suffix) {
   node.id = newId;
   for (var i = 0; i < node.children; i++) {
-    updateId(newId * 10 + suffix + 1, node.children[i], suffix);
+    updateId(newId * 10 + suffix, node.children[i], suffix);
   }
 }
 
