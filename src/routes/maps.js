fb4b561c889fac0a1e3a8d4fd2c3cef7f6d707f8
mrydzy <majrydzy@gmail.com>
2016-04-09 23:49:29 +0200

    listing maps

--- src/routes/maps.js
+++ src/routes/maps.js
@@ -5,23 +5,22 @@ const authUrl = require('../client/service/constants').authUrl;
 
 module.exports = (passport) => {
 
-  router.get('/', function(req, res) {
-    const backendUrl = req.app.locals.settings.cfg.API_URI + "/trees";
-
+  router.get('/list/:lang?', function(req, res) {
+    const backendSuffix = req.params.lang ? '/'+ encodeURIComponent(req.params.lang) + '/lang' : '';
+    var backendUrl = req.app.locals.settings.cfg.API_URI + "/trees" + backendSuffix;
     fetch(backendUrl)
       .then(function(response) {
         if (response.status >= 400) {
-          throw new Error("Bad response from server");
-          res.send(500);
+          res.status(500).send('Error - check the requested language');
+          throw new Error("Bad response from server", response);
         }
         return response.json();
       })
       .then(function(response) {
-        res.render('list-maps', { mapList: response});
+        res.render('list-maps', { mapList: response, lang: req.params.lang});
       })
       .catch(e => res.send(e));
   });
-
 
   router.get('/show/:id', function(req, res) {
     const backendUrl = req.app.locals.settings.cfg.API_URI + "/trees";
@@ -31,8 +30,8 @@ module.exports = (passport) => {
     fetch(backendUrl + '/' + treeId)
       .then(function(response) {
         if (response.status >= 400) {
-          throw new Error("Bad response from server");
-          res.send(500);
+          res.status(500).send('Unexpected Error');
+          throw new Error("Bad response from server", response);
         }
         return response.json();
       })
